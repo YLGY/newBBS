285a3e200fedebc369814a623a254f5fef182954
YLGY <rlgcyy@gmail.com>
2018-07-21 12:20:34 +1000

    Finish user auth

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,19 +1,43 @@
 <header class="header">
     <div class="header__logo header__item">
-        newBBS
+        <a href="{{ url('/') }}">
+            newBBS
+        </a>
     </div>
     <div class="header__right header__item">
         <ul class="header__nav">
-            <li class="header__list">
-                <a href="#" class="header__link">
-                    Login
-                </a>
-            </li>
-            <li class="header__list">
-                <a href="#" class="header__link">
-                    Register
-                </a>
-            </li>
+            @guest
+                <li class="header__list">
+                    <a href="{{ route('login') }}" class="header__link">
+                        Login
+                    </a>
+                </li>
+                <li class="header__list">
+                    <a href="{{ route('register') }}" class="header__link">
+                        Register
+                    </a>
+                </li>
+            @else
+                <li class="header__list">
+                    <a href="#" class="header__link">
+                        <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" 
+                            class="header__img" width="30px" height="30px"
+                        >
+                        {{ Auth::user()->name }}
+                    </a>
+                </li>
+                <li class="header__list">
+                    <a href="{{ route('logout') }}" class="header__link"
+                        onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();"   
+                    >
+                        Logout
+                    </a>
+                    <form action="{{ route('logout') }}" id="logout-form" style="display:none;" method="POST">
+                        @csrf
+                    </form>
+                </li>
+            @endguest
         </ul>
     </div>
 </header>
