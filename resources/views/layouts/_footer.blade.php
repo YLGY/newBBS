44c5747aefbcae3991b673ea58c2f78659524e4f
YLGY <rlgcyy@gmail.com>
2018-07-21 11:30:57 +1000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="left">
+        刻意练习，每日精进
+    </div>
+    <div class="time">
+        &copy;{{ date('Y') }}
+    </div>
+    <div class="right">
+        <ul>
+            <li>
+                <a href="#">About Us</a>
+            </li>
+        </ul>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,19 @@
+<header class="header">
+    <div class="header__logo header__item">
+        newBBS
+    </div>
+    <div class="header__right header__item">
+        <ul class="header__nav">
+            <li class="header__list">
+                <a href="#" class="header__link">
+                    Login
+                </a>
+            </li>
+            <li class="header__list">
+                <a href="#" class="header__link">
+                    Register
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
