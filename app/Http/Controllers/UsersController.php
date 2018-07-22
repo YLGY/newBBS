6118e26a22f53f050586cc7772b2fc03314dde7a
YLGY <rlgcyy@gmail.com>
2018-07-22 15:54:57 +1000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,7 @@ class UsersController extends Controller
             'except' => ['show']
         ]);
     }
-    
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -24,11 +24,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
