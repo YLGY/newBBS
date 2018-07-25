cf87d0afe4e76aa65c576c7574c1befbb5eae478
YLGY <rlgcyy@gmail.com>
2018-07-25 15:15:29 +1000

    WYSIWYG editor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -44,7 +44,7 @@
                         </div>
                         <div class="form-group">
                             <label for="body-field">Content</label>
-                            <textarea name="body" id="body-field" class="form-control" rows="3">{{ old('body', $topic->body ) }}</textarea>
+                            <textarea name="body" id="editor" class="form-control" rows="3" required>{{ old('body', $topic->body ) }}</textarea>
                         </div> 
 
                         <div class="well well-sm">
@@ -56,4 +56,23 @@
     </div>
 </div>
 
+@endsection
+
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/simditor.css') }}" type="text/css">
+@endsection
+
+@section('scripts')
+    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            var editor = new Simditor({
+                textarea: $('#editor'),
+            });
+        });
+    </script>
 @endsection
