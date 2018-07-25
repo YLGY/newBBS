15d90bafe399729919f4398cdeb13dd0c80ae4a8
YLGY <rlgcyy@gmail.com>
2018-07-25 22:21:10 +1000

    Topic detail page

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -1,60 +1,43 @@
 @extends('layouts.app')
+@section('title', $topic->title)
+@section('description', $topic->excerpt)
 
 @section('content')
 
-<div class="container">
-    <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h1>Topic / Show #{{ $topic->id }}</h1>
-            </div>
+<div class="topic">
+    <div class="topic__author">
+        <p class="topic__author-name">
+            Author: {{ $topic->user->name }}
+        </p>
+        <div class="topic__author-avatar">
+            <a href="{{ route('users.show', $topic->user->id) }}">
+                <img src="{{ $topic->user->avatar }}" alt="{{ $topic->user->name }}">
+            </a>
+        </div>
+    </div>
 
-            <div class="panel-body">
-                <div class="well well-sm">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <a class="btn btn-link" href="{{ route('topics.index') }}"><i class="glyphicon glyphicon-backward"></i> Back</a>
-                        </div>
-                        <div class="col-md-6">
-                             <a class="btn btn-sm btn-warning pull-right" href="{{ route('topics.edit', $topic->id) }}">
-                                <i class="glyphicon glyphicon-edit"></i> Edit
-                            </a>
-                        </div>
-                    </div>
-                </div>
+    <div class="topic__content">
+        <h1 class="topic__header">
+            {{ $topic->title }}
+        </h1>
+        <div class="topic__sub">
+            {{ $topic->created_at->diffForHumans() }}
+            <span class="media__span"> • </span>
+            {{ $topic->reply_count }} Replies
+        </div>
 
-                <label>Title</label>
-<p>
-	{{ $topic->title }}
-</p> <label>Body</label>
-<p>
-	{{ $topic->body }}
-</p> <label>User_id</label>
-<p>
-	{{ $topic->user_id }}
-</p> <label>Category_id</label>
-<p>
-	{{ $topic->category_id }}
-</p> <label>Reply_count</label>
-<p>
-	{{ $topic->reply_count }}
-</p> <label>View_count</label>
-<p>
-	{{ $topic->view_count }}
-</p> <label>Last_reply_user_id</label>
-<p>
-	{{ $topic->last_reply_user_id }}
-</p> <label>Order</label>
-<p>
-	{{ $topic->order }}
-</p> <label>Excerpt</label>
-<p>
-	{{ $topic->excerpt }}
-</p> <label>Slug</label>
-<p>
-	{{ $topic->slug }}
-</p>
-            </div>
+        <div class="topic__body">
+            {!! $topic->body !!}
+        </div>
+
+        <div class="topic__footer">
+            <hr>
+            <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-primary btn-x" role="button">
+                Edit
+            </a>
+            <a href="#" class="btn btn-primary btn-xs" role="button">
+                Delete
+            </a>
         </div>
     </div>
 </div>
